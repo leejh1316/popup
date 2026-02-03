c981cb2fe251cf9986949455039e4ca3be5214c5
leejh1316 <dlwogurtkfkd@gmail.com>
2026-02-04 01:20:48 +0900

    github action 세팅

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,21 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "@components": path.resolve(__dirname, "./src/components"),
-      "@pages": path.resolve(__dirname, "./src/pages"),
-      "@lib": path.resolve(__dirname, "./src/lib"),
+export default defineConfig(() => {
+  const baseUrl = "/vue-popup/";
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+        "@components": path.resolve(__dirname, "./src/components"),
+        "@pages": path.resolve(__dirname, "./src/pages"),
+        "@lib": path.resolve(__dirname, "./src/lib"),
+      },
     },
-  },
+    define: {
+      __BASE_URL__: JSON.stringify(baseUrl),
+    },
+    base: baseUrl,
+  };
 });
